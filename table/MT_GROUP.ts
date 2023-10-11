a454410f8ad7699fa0d27826e3423e2643096409
tally54 <sikakou0504@gmail.com>
2023-10-11 17:46:57 +0900

    招待コードの構造を追加

--- table/MT_GROUP.ts
+++ table/MT_GROUP.ts
@@ -2,5 +2,9 @@ export interface MT_GROUP {
     country: number,
     id: string,
     name: string,
-    userId: string
+    userId: string,
+    invi: {
+        id: string,
+        password: string
+    }
 }
